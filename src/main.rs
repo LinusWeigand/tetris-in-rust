c3d0c12db426d52004ccd50130156e90a536aa5c
Linus Weigand <linusweigand@protonmail.com>
2022-03-28 14:28:52 +0200

    does_piece_fit()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-
+const FIELDWIDTH: usize = 12;
+const FIELDHEIGHT: usize = 18;
 
 fn main() {
     let tetromino: [&str; 7]
@@ -10,23 +11,12 @@ fn main() {
     ".....XX...X...X.",
     ".....XX..X...X.."];
 
-    const FIELDWIDTH: usize = 12;
-    const FIELDHEIGHT: usize = 18;
+   
 
     let mut field: [usize; FIELDWIDTH*FIELDHEIGHT] = [0; FIELDWIDTH*FIELDHEIGHT];
     for x in 0..FIELDWIDTH {
         for y in 0..FIELDHEIGHT {
             field[y * FIELDWIDTH + x] = if x == 0 || x == FIELDWIDTH -1 || y == FIELDHEIGHT -1 {9} else {0};
-        }
-    }
-
-    fn rotate(x: usize, y: usize, r: usize) -> usize {
-        return match r % 4 {
-            0 => y * 4 + x,
-            1 => 12 + y - (x * 4),
-            2 => 15 - (y * 4) - y,
-            3 => 3 - y + (x * 4),
-            _ => 0
         }
     }
 
@@ -65,6 +55,39 @@ fn main() {
             println!("{}", row);
         }
     }
+}
+
+fn rotate(x: usize, y: usize, r: usize) -> usize {
+    return match r % 4 {
+        0 => y * 4 + x,
+        1 => 12 + y - (x * 4),
+        2 => 15 - (y * 4) - y,
+        3 => 3 - y + (x * 4),
+        _ => 0
+    }
+}
+
+fn does_piece_fit(field: [usize; FIELDWIDTH*FIELDHEIGHT], tetromino: [&str; 7], ntetromino: usize, rotation: usize, x: usize, y: usize) -> bool {
+        
+    for px in 0..4 {
+        for py in 0..4 {
+            //index with rotation
+            let pi = rotate(px, py, rotation);
+            //index without rotation
+            let fi = (y + py) * FIELDWIDTH + (x + px); 
+
+            //out of bounds?
+            if x + px < FIELDWIDTH {
+                if  y + py < FIELDHEIGHT {
+                    if tetromino[ntetromino].as_bytes()[pi] == b'X' && field[fi] != 0 {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    
+    return true;
 }
 
 
